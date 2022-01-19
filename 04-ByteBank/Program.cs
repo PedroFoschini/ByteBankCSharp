45c761266eed65c430a5d119435b1b75515ede6e
Pedro Foschini <plfoschini@gmail.com>
2022-01-19 09:43:40 -0300

    Fim aula 3

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+            bool resultadoSaque = contaDoBruno.Sacar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+
+            contaDoBruno.Depositar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Saldo Bruno antes transferência: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo Gabriela antes transferência: " + contaDaGabriela.saldo);
+
+            bool resultadoTransferencia = contaDoBruno.Transferir(200, contaDaGabriela);
+
+            Console.WriteLine("Saldo Bruno após transferência: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo Gabriela após transferência: " + contaDaGabriela.saldo);
+
+            Console.WriteLine(resultadoTransferencia);
+
+            Console.ReadLine();
+        }
+    }
+}
