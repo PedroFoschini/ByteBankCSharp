50a15baa6b330a3e61b2e85011e5be397678b185
Pedro Foschini <plfoschini@gmail.com>
2022-02-09 15:44:55 -0300

    Final do curso

---
+++ 07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,99 @@
+﻿// using _05_ByteBank;
+
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        private Cliente _titular;
+
+        public static int totalDeContas { get; private set; }
+
+        private int _agencia;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+
+                _agencia = value;
+            }
+        }
+
+        
+        private int _numero;
+        private double _saldo = 100.0;
+
+
+        public Cliente Titular { get; set; }
+
+        public int Numero { get; set; }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            this.Agencia = agencia;
+            this.Numero = numero;
+
+            totalDeContas++;
+        }
+
+
+        public double Saldo 
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+
+                _saldo = value;
+            }
+        }
+
+
+        public bool Sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+            return true;
+
+        }
+
+        public void Depositar(double valor)
+        {
+            this._saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+            }
+
+            this._saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+
+        }
+
+    }
+}
+
+
+
